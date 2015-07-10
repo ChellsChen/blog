bea3cf2ee59646bdbd5bb705d728517326f4bcc8
陈小雪 <xiaoxue.chen@fraudmetrix.cn>
2015-07-10 13:35:30 +0800

    optimize ui

--- web/static/js/layout.js
+++ web/static/js/layout.js
@@ -1,27 +1,70 @@
 var CLASS;
 var INDEX;
+var TAGS;
 var INDEX_URL = "/store/index.json"
 var CLASS_URL = "/store/class.json"
+var TAGS_URL = " /store/tags.json"
 
 function datainit(){
+    INDEX = getJsonData(INDEX_URL);
+    CLASS = getJsonData(CLASS_URL);
+    TAGS = getJsonData(TAGS_URL);
+}
 
+function getJsonData(url){
+    var jsondata;
     $.ajax({
-        url:INDEX_URL,
-        aysnc:false,
+        url:url,
+        async:false,
         dataType:"json",
         success:function(data){
-            CLASS = data;
-
-        },
+            jsondata = data;
+        }
     });
+    return jsondata;
+}
 
-    $.ajax({
-        url:CLASS_URL,
-        aysnc:false,
-        dataType:"json",
-        success:function(data){
-            INDEX = data;
-        };
+
+function show(){
+    showclass();
+    showmenu();
+}
+
+function showclass(){
+    var label_list = [
+        "label-primary",
+        "label-success",
+        "label-info",
+        "label-warning",
+        "label-danger",
+        "label-default",
+        ];
+    var str = "";
+    var i = 0;
+    $.each(CLASS,function(key, value){
+        str += "<span class='label "+label_list[i]+" cls-l-span' name='"+key+"'>"+key+"</span><br>";
+        i++;
     });
+    $(".cls-l-div").html(str);
+}
 
+function showmenu(cls){
+    var str = "";
+    var href = "/chapter.html?id=";
+    if(cls){
+        var indexs = CLASS[cls];
+        $.each(indexs, function(index, number){
+            href += number;
+            var title = INDEX[index];
+
+            str += "<a class='menu-list-link list-group-item list-group-item-info' href='" + href + "''>" + title + "</a><br>";
+        });
+    }
+    else{
+        $.each(INDEX, function(key, value){
+            var h = href + key;
+            str += "<a class='menu-list-link list-group-item list-group-item-info' href='" + h + "''>" + value[0] + "</a><br>";
+        });
+    }
+    $(".menu-div").html(str);
 }
